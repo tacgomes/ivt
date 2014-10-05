38c6a981517764b0128a0ae530c4be9cc0f28563
Tiago Gomes <tiago.gomes@codethink.co.uk>
2014-10-05 17:19:09 +0100

    Add support for testing missing required parameters
    Change the spec so that a key and value setting is separated by a
    equals sign instead of space. This allows to have settings that
    don't have a correspondent value associated (e.g. --help).
    
    Also change example.py to have a concept of subcommands.

--- example.py
+++ example.py
@@ -8,24 +8,39 @@ def raise_invalid_value():
     sys.stderr.write("Error: invalid value\n")
     sys.exit(1)
 
+def raise_missing_value():
+    sys.stderr.write("Error: missing value\n")
+    sys.exit(2)
+
 def main():
     parser = optparse.OptionParser()
     parser.add_option('--hour', action="store", type="int")
     parser.add_option('--username', action="store")
     parser.add_option('--day', action="store")
-    options, files = parser.parse_args()
+    options, args = parser.parse_args()
 
-    if options.hour:
-        if options.hour < 0 or options.hour > 24:
-            raise_invalid_value()
+    subcommand=args[0]
+    if subcommand == 'set-time':
+        if options.hour:
+            if options.hour < 0 or options.hour > 24:
+                raise_invalid_value()
+        else:
+            raise_missing_value()
 
-    if options.day:
-        if options.day not in ['mon','tue','wed','thu','fri', 'sat', 'sun']:
-            raise_invalid_value()
+        if options.day:
+            if options.day not in ['mon','tue','wed','thu',
+                                   'fri', 'sat', 'sun']:
+                raise_invalid_value()
+    elif subcommand == 'adduser':
+        if options.username:
+            if not re.match('[a-z_][a-z0-9_]{0,30}', options.username):
+                raise_invalid_value()
+        else:
+            raise_missing_value()
+    else:
+        sys.stderr.write("Error: invalid value\n")
+        sys.exit(1)
 
-    if options.username or (not options.username and options.username == '') :
-        if not re.match('[a-z_][a-z0-9_]{0,30}', options.username):
-            raise_invalid_value()
 
 if __name__ == "__main__":
     main()
--- example.spec
+++ example.spec
@@ -4,7 +4,5 @@ regex username=[a-z_][a-z0-9_]{0,30}
 range hour=0..24
 enum  day=mon,tue,wed,thu,sat,sun
 
-#./example.py --hour $hour
-#./example.py --username $username
-#./example.py --day $day
-./example.py foo [ --day $day ] [ --hour $hour ]
+./example.py set-time --hour=$hour [ --day=$day ]
+#./example.py adduser --username=$username
