fb3cc31fe8e861990194c44e238c2a064927d793
Tiago Gomes <tiago.gomes@codethink.co.uk>
2014-10-12 23:04:28 +0100

    Modernize: switch to Python 3 and argparse

--- example.py
+++ example.py
@@ -1,57 +1,58 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import re
-import optparse
+import argparse
 
 def raise_invalid_value(option, value):
-    sys.stderr.write("Error: invalid value '{0}' for '{1}'\n".format(
+    sys.stderr.write("Error: invalid value '{}' for '{}'\n".format(
                      value, option))
     sys.exit(1)
 
 def raise_missing_option(option):
-    sys.stderr.write("Error: option '{0}] was not give\n".format(option))
+    sys.stderr.write("Error: option '{}' was not given\n".format(option))
     sys.exit(2)
 
 def main():
-    parser = optparse.OptionParser()
-    parser.add_option('--hour', action="store", type="int")
-    parser.add_option('--minute', action="store", type="int")
-    parser.add_option('--second', action="store", type="int")
-    parser.add_option('--timezone', action="store")
-    parser.add_option('--username', action="store")
-    parser.add_option('-v', '--verbosity', action="store")
-    options, args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('subcommand')
+    parser.add_argument('--hour', action="store", type=int)
+    parser.add_argument('--minute', action="store", type=int)
+    parser.add_argument('--second', action="store", type=int)
+    parser.add_argument('--timezone', action="store")
+    parser.add_argument('--username', action="store")
+    parser.add_argument('-v', '--verbosity', action="store")
+    namespace = parser.parse_args()
 
-    subcommand=args[0]
+    subcommand=namespace.subcommand
     if subcommand == 'show-log':
          pass
-         if options.verbosity:
-             if options.verbosity not in ['info','warn','error']:
-                 raise_invalid_value('verbosity', options.verbosity)
+         if namespace.verbosity:
+             if namespace.verbosity not in ['info','warn','error']:
+                 raise_invalid_value('verbosity', namespace.verbosity)
     elif subcommand == 'set-time':
-        if options.hour:
-            if options.hour < 0 or options.hour > 24:
-                raise_invalid_value('hour', options.hour)
+        if namespace.hour:
+            if namespace.hour < 0 or namespace.hour > 24:
+                raise_invalid_value('hour', namespace.hour)
         else:
             raise_missing_option('hour')
-        if options.minute:
-            if options.minute < 0 or options.minute > 60:
-                raise_invalid_value('minute', options.minute)
+        if namespace.minute:
+            if namespace.minute < 0 or namespace.minute > 60:
+                raise_invalid_value('minute', namespace.minute)
         else:
             raise_missing_option('minute')
-        if options.second:
-            if options.second < 0 or options.second > 60:
-                raise_invalid_value('second', options.second)
+        if namespace.second:
+            if namespace.second < 0 or namespace.second > 60:
+                raise_invalid_value('second', namespace.second)
         else:
             raise_missing_option('second')
-        if options.timezone is not None:
-            if not re.match('\w+$', options.timezone):
-                raise_invalid_value('timezone', options.timezone)
+        if namespace.timezone is not None:
+            if not re.match('\w+$', namespace.timezone):
+                raise_invalid_value('timezone', namespace.timezone)
     elif subcommand == 'add-user':
-        if options.username is not None:
-            if not re.match('[a-z_][a-z0-9_]+$', options.username):
-                raise_invalid_value('username', options.username)
+        if namespace.username is not None:
+            if not re.match('[a-z_][a-z0-9_]+$', namespace.username):
+                raise_invalid_value('username', namespace.username)
         else:
             raise_missing_option('username')
     elif subcommand == 'help':
