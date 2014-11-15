fc2edd3b278664b0c85f2c8c9b7b2584e2ea7c69
Tiago Gomes <tiago.gomes@codethink.co.uk>
2014-11-15 10:30:47 +0000

    Don't check if options marked as required were given
    The argparse library will raise an error if not.

--- example.py
+++ example.py
@@ -11,12 +11,6 @@ def invalid_value_err(option, value):
     sys.exit(1)
 
 
-def missing_option_err(option):
-    sys.stderr.write("Error: option '{}' was not given\n".format(
-                     option))
-    sys.exit(2)
-
-
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="subcommand")
@@ -48,30 +42,18 @@ def main():
     if namespace.subcommand == 'show-log':
          pass
     elif namespace.subcommand == 'set-time':
-        if namespace.hour:
-            if namespace.hour < 0 or namespace.hour > 24:
-                invalid_value_err('hour', namespace.hour)
-        else:
-            missing_option_err('hour')
-        if namespace.minute:
-            if namespace.minute < 0 or namespace.minute > 60:
-                invalid_value_err('minute', namespace.minute)
-        else:
-            missing_option_err('minute')
-        if namespace.second:
-            if namespace.second < 0 or namespace.second > 60:
-                invalid_value_err('second', namespace.second)
-        else:
-            missing_option_err('second')
+        if namespace.hour < 0 or namespace.hour > 24:
+            invalid_value_err('hour', namespace.hour)
+        if namespace.minute < 0 or namespace.minute > 60:
+            invalid_value_err('minute', namespace.minute)
+        if namespace.second < 0 or namespace.second > 60:
+            invalid_value_err('second', namespace.second)
         if namespace.timezone is not None:
             if not re.match('\w+$', namespace.timezone):
                 invalid_value_err('timezone', namespace.timezone)
     elif namespace.subcommand == 'add-user':
-        if namespace.username is not None:
-            if not re.match('[a-z_][a-z0-9_]+$', namespace.username):
-                invalid_value_err('username', namespace.username)
-        else:
-            missing_option_err('username')
+        if not re.match('[a-z_][a-z0-9_]+$', namespace.username):
+            invalid_value_err('username', namespace.username)
     elif namespace.subcommand == 'help':
          pass
     else:
